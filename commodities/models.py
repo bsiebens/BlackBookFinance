ad37d957c68ebfea8b9c4b42b4c0f7c3cdc4be2b
Bernard Siebens <bernard@siebens.org>
2025-07-30 21:39:45 +0200

    Reorganize imports in `Commodity` model for readability and consistency

--- commodities/models.py
+++ commodities/models.py
@@ -1,9 +1,11 @@
 from collections import defaultdict, deque
-from django.db.models import Max
-from django.db import models
-from django.utils.translation import gettext_lazy as _
-from django.utils import timezone
 from decimal import Decimal
+
+from django.db import models
+from django.db.models import Max
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
 
 class Commodity(models.Model):
     """
