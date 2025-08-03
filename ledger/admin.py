b1fbd5a7582fd3d73aee391936cc83cdaab6eea7
Bernard Siebens <bernard@siebens.org>
2025-08-03 23:21:55 +0200

    Initialize `ledger` models with `Bank`, `Account`, and migrations; configure admin interface

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,1 +1,16 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Bank, Account
+
+
+@admin.register(Bank)
+class BankAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+    ordering = ["name"]
+    fieldsets = [
+        ["GENERAL INFORMATION", {"fields": ["name"], "classes": ["wide"]}],
+    ]
+
+
+admin.site.register(Account)
--- ledger/models.py
+++ ledger/models.py
@@ -1,1 +1,59 @@
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from tree_queries.models import TreeNode
+
+from commodities.models import Commodity
+
+
+class Bank(models.Model):
+    """
+    Represents a bank entity used in financial or organizational contexts.
+
+    This class defines the structure and behavior of a bank, allowing
+    it to be uniquely identified by its name. It supports Django's ORM
+    functionality by inheriting from the `models.Model` class and includes
+    localization support for internationalized applications.
+
+    :ivar name: The unique name of the bank.
+    :type name: str
+    """
+
+    name = models.CharField(_("name"), max_length=250, unique=True)
+
+    class Meta:
+        verbose_name = _("bank")
+        verbose_name_plural = _("banks")
+
+    def __str__(self):
+        return self.name
+
+
+class Account(TreeNode):
+    class AccountTypes(models.TextChoices):
+        ASSETS = "assets", _("Assets")
+        LIABILITIES = "liabilities", _("Liabilities")
+        EXPENSES = "expenses", _("Expenses")
+        INCOME = "income", _("Income")
+        EQUITY = "equity", _("Equity")
+        CASH = "cash", _("Cash")
+        OTHER = "other", _("Other")
+
+    name = models.CharField(_("name"), max_length=250)
+    type = models.CharField(_("type"), max_length=15, choices=AccountTypes.choices, default=AccountTypes.OTHER)
+    bank = models.ForeignKey(Bank, verbose_name=_("bank"), on_delete=models.SET_NULL, blank=True, null=True, related_name="accounts")
+    default_currency = models.ForeignKey(
+        Commodity,
+        limit_choices_to={"commodity_type": Commodity.CommodityTypes.CURRENCY},
+        verbose_name=_("default currency"),
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+
+    created = models.DateTimeField(_("created"), auto_now_add=True)
+    updated = models.DateTimeField(_("updated"), auto_now=True)
+
+    class Meta:
+        verbose_name = _("account")
+        verbose_name_plural = _("accounts")
+        constraints = [models.UniqueConstraint(fields=["name", "parent"], name="unique_account_name_per_parent")]
